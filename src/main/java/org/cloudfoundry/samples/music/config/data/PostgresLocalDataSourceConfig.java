7b4eae3780c376e6cf72f96fa6349effd2a9e781
jensonjoseph <jenson.joseph@outlook.com>
2018-03-10 20:48:31 -0500

    pointing to my local postgres

--- src/main/java/org/cloudfoundry/samples/music/config/data/PostgresLocalDataSourceConfig.java
+++ src/main/java/org/cloudfoundry/samples/music/config/data/PostgresLocalDataSourceConfig.java
@@ -12,7 +12,7 @@ public class PostgresLocalDataSourceConfig extends AbstractLocalDataSourceConfig
 
     @Bean
     public DataSource dataSource() {
-        return createDataSource("jdbc:postgresql://localhost/music",
+        return createDataSource("jdbc:postgresql://localhost:5432/postgres",
                 "org.postgresql.Driver", "postgres", "postgres");
     }
 
